8146b726106d6c80e6563f9df10f29770a06d72e
cansancaojennifer <cansancaojennifer@gmail.com>
2023-07-18 15:29:57 -0300

    Continuação do curso

---
+++ Aulas/aula3.py
@@ -0,0 +1,21 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Texto qualquer.')
+print(1, '"Texto" qualquer.')
+
+# Aspas duplas
+print("Outro texto qualquer.")
+print(2, "Outro 'texto' qualquer.")
+# Escape
+print("Outro \"texto qualquer 2.")
+
+# r
+print(r"Outro \"texto qualquer 2.")
