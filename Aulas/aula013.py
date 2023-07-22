f6a9fbf8859bc261c974dd70f82e3e2eb5cf16c4
cansancaojennifer <cansancaojennifer@gmail.com>
2023-07-22 13:33:58 -0300

    f strings

---
+++ Aulas/aula013.py
@@ -0,0 +1,8 @@
+nome = 'Paciente 1'
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu imc é {imc:.2f}'
+print(linha_1)
