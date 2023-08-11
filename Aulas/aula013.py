504aa2516b1d6c667408e39c017516d978f05b7a
cansancaojennifer <cansancaojennifer@gmail.com>
2023-08-11 18:13:51 -0300

    update aula013

--- Aulas/aula013.py
+++ Aulas/aula013.py
@@ -3,6 +3,7 @@ altura = 1.80
 peso = 95
 imc = peso / (altura * altura)
 
-"f-strings"
+#f-strings
 linha_1 = f'{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu imc é {imc:.2f}'
+
 print(linha_1)
