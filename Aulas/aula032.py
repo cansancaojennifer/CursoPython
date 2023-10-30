e9eab5dd65fb388660ae3fdbadd63a6b7ca208fb
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-10-30 16:42:15 -0300

    exercicios

--- Aulas/aula032.py
+++ Aulas/aula032.py
@@ -1,3 +1,5 @@
+#Exercicio 01
+
 numero = input('Digite um numero inteiro: ')
 
 
@@ -10,3 +12,36 @@ if numero.isdigit():
         numero = int(numero)
 else:
         print("Você não digitou um número inteiro")
+
+#Exercicio 02
+
+entrada = input('Digite a hora em números inteiros: ')
+
+try:
+    hora = int(entrada)
+
+    if hora >= 0 and hora <= 11:
+        print('Bom dia')
+    elif hora >= 12 and hora <= 17:
+        print('Boa tarde')
+    elif hora >= 18 and hora <= 23:
+        print('Boa noite')
+    else:
+        print('Não conheço essa hora')
+except:
+    print('Por favor, digite apenas números inteiros')
+
+# Exercicio 03
+
+nome = input('Digite seu nome: ')
+tamanho_nome = len(nome)
+
+if tamanho_nome > 1:
+    if tamanho_nome <= 4:
+        print('Seu nome é curto')
+    elif tamanho_nome >= 5 and tamanho_nome <= 6:
+        print('Seu nome é normal')
+    else:
+        print('Seu nome é muito grande')
+else:
+    print('Digite mais de uma letra.')
