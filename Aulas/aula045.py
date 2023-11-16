222924a807207f338084fac8f7ded234ecbd7d81
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-11-16 16:03:39 -0300

    iter

---
+++ Aulas/aula045.py
@@ -0,0 +1,14 @@
+# for letra in texto
+texto = 'Luiz'  # iterável
+
+# iteratador = iter(texto)  # iterator
+
+# while True:
+#     try:
+#         letra = next(iteratador)
+#         print(letra)
+#     except StopIteration:
+#         break
+
+for letra in texto:
+    print(letra)
